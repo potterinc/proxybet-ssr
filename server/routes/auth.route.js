7ed7d9ee7c0342fe2c6fa1ab0e0091eda02130f8
Arekhandia Harry <harry_p30@yahoo.com>
2022-10-25 23:15:23 +0100

    AUTHENTICATION Route
    configured router links without logic

---
+++ server/routes/auth.route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const authentication = express.Router()
+
+authentication
+    .post('/login', (req, res) => {
+        res.status(200).json({ status: 'ok' })
+    })
+    .post('/register', (req, res) => {
+        res.status(201).json({ status: 'ok' })
+    })
+    .post('/:email', (req, res) => {
+        
+        res.status(200).json({email: req.params.email, status: 'ok'})
+    })
+    .post('/reset', (req, res)=>{
+        res.status(200).json({status: 'ok'})
+    })
+
+module.exports = authentication;
