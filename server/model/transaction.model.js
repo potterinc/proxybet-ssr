4e8720841f83b98f6737483d45c6e2027337d544
Arekhandia Harry <harry_p30@yahoo.com>
2022-10-28 23:32:00 +0100

    Initialilzed Bet and Transaction model

---
+++ server/model/transaction.model.js
@@ -0,0 +1,10 @@
+const {mongoose} = require ('mongoose')
+
+const PaymentSchema = mongoose.Schema;
+
+const PaymentModel = new PaymentSchema({
+    
+})
+
+
+module.exports = monggose.model('Transaction', paymentModel)
