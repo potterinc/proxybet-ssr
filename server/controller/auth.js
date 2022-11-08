43c614b5ac5f024a8f17f55eb1483b6684a0b74d
Arekhandia Harry <harry_p30@yahoo.com>
2022-11-08 13:49:10 +0100

    Added welcome email to user

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -30,7 +30,39 @@ const newUser = (req, res) => {
                     status: true,
                     token: SIGN_AUTH_TOKEN(user, process.env.ACCESS_TOKEN_SECRET)
                 })
-
+                const d = new Date()
+                let year = d.getFullYear()
+                let mail = `
+                <h1 style="text-align:center">Welcome ${user.firstName},</h1>
+                <p style="text-align:center">
+                Thanks for signing up with ProxyBET, we're so happy to have you
+                on board. You are now one step closer to becoming an bet king!<br>
+                Your account is Activated!</p>
+                
+                <div style="text-align:center; margin: 50px 0">
+                <a 
+                    href="https://proxybet.com/api/user/profile" 
+                    target="_blank"
+                    style="
+                        width: 70%; 
+                        padding:15px; 
+                        background: #008080;
+                        text-decoration: none;
+                        text-align:center;
+                        border-radius: 5px;
+                        margin: 20px;
+                        box-shadow: 0 1px 2px #333;
+                        font-weight: bold;
+                        color: #fff"
+                >Click Here to Fund Wallet</a>
+                </div>
+                <hr>
+                 <small>
+                    <p>&copy; ${year} <a style="color: #008080; text-decoration: none" href="https://proxybet.com" target="_blank">ProxyBet</a>. All rights reserved.</p>
+                </small>
+                <p>
+                `
+                proxyMailer('One step closer to unlimited winning', mail, user.email)
             })
             .catch(e => {
                 if (e.name == 'ValidationError') {
@@ -127,14 +159,56 @@ const SIGN_AUTH_TOKEN = (payload, key) => {
     return jwt.sign({ payload }, key, { expiresIn: '7d' })
 }
 
+/**
+ * Sending email to user
+ * @param {String} subject message subject header
+ * @param {String | HTMLCollection} msgContent HTML string or plain text
+ * @param {String } recepient Receiving email addreess
+ */
+const proxyMailer = (subject, msgContent, recepient) => {
+
+    const mailer = nodemailer.createTransport({
+        host: process.env._SMTP_HOST,
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env._SMTP_USER,
+            pass: process.env._PASSKEY,
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false,
+        },
+    });
+
+    const mailOptions = {
+        from: 'ProxyBet <harek@potterincorporated.com>',
+        to: recepient,
+        subject, // same output in plain text format
+        html: msgContent
+    };
+
+    mailer.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            res.sendStatus(501)
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.sendStatus(200)
+        }
+    });
+}
 
 const RESET_CODE = () => {
     const randomString = crypto.randomBytes(3).toString("hex").toUpperCase();
     return randomString
 
 }
+/** 
+ *MIDDLEWARE FUNCTION
+ */
 
-// Authentication Middleware function
+// AUTHORIZATION TOKEN
 const VERIFY_AUTH_TOKEN = (req, res, next) => {
     const bearerHeader = req.headers['authorization']
     if (typeof bearerHeader !== 'undefined') {
