44e2d499ae0de5c80fa7b07f9583dcf5ae064cca
Arekhandia Harry <harry_p30@yahoo.com>
2024-09-12 15:29:35 +0100

    [fix]
    Fixed validation error handling on update password

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -55,16 +55,16 @@ class AuthController {
 
   /** @description Update new password */
   async updatePassword(req: Request, res: Response, next: NextFunction) {
-    const user: { id: string, password: string } = {
-      id: req.body.id,
-      password: hashSync(req.body.password, 3)
-    }
-
-    const authService = new AuthenticationService(user)
     try {
-      if (!user.id || !user.password)
+      const user: { id: string, password: string } = {
+        id: req.body.id,
+        password: hashSync(req.body.password, 3)
+      }
+
+      const authService = new AuthenticationService(user)
+      if (!user.password)
         throw new ValidationError('Password required');
-      
+
       await authService.updatePassword()
         .then(() => {
           res.status(202).json({
